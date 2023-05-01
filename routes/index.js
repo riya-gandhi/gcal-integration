dd334b604f8391ac8b5ae48f710b04f76691e33b
Riya Gandhi <63222339+riya-gandhi@users.noreply.github.com>
2023-05-01 10:55:37 +0530

    Init: render homepage

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = new express.Router();
 const path = require("path");
-const publicDicrectoryPath = path.join(__dirname, "../public");
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-router.get("/my-account", (req, res) => {
-  res.sendFile(publicDicrectoryPath + "/my-google-events.html");
+router.get("/", (req, res) => {
+  res.sendFile(publicDirectoryPath + "/my-google-events.html");
+  // this renders html file on default route
 });
 
 module.exports = router;
